20d29d3b0eeb6db42969b12cf490990c2485d689
Hamza Mahmood <hamzamahmood195@gmail.com>
2025-11-24 16:39:30 +0000

    day 1 quiz project complete

--- src/quiz.js
+++ src/quiz.js
@@ -1,20 +1,53 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor(questions, timeLimit, timeRemaining) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return this.currentQuestionIndex++;
+  }
+
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const random = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[random]] = [
+        this.questions[random],
+        this.questions[i],
+      ];
     }
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    for (let i = 0; i < this.questions.length; i++) {
+      const currentQuestion = this.questions[i];
+      if (answer === currentQuestion.answer) {
+        return this.correctAnswers++;
+      }
+    }
+  }
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 }
